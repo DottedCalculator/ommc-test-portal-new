5834081019d537def12b84c859ef672b01b544eb
DottedCalculator <112578558+DottedCalculator@users.noreply.github.com>
2026-09-10 20:09:58 -0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -50,7 +50,7 @@ const Home: NextPage = () => {
               <h5 className="z-10 text-center text-3xl font-extrabold sm:text-4xl md:text-5xl lg:mb-8 lg:text-6xl xl:text-7xl">
                 Welcome to{" "}
                 <span className="block rounded-3xl bg-red-600 px-3 py-1 text-white dark:bg-red-700 sm:inline">
-                  OMMC Year 5
+                  OMMC Year 6
                 </span>
               </h5>
               <button
